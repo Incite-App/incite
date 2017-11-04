9ef0ce396da6f369098fa9be2dc2019fb588b955
Geoff Tripoli <gtripoli@gmail.com>
2017-11-04 14:26:04 -0700

    Added angular material and firebase

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyB8CAh3Cddj9SQdadar8ULN9nLFGXF2fCU',
+      authDomain: 'incite-10624.firebaseapp.com',
+      databaseURL: 'https://incite-10624.firebaseio.com',
+      projectId: 'incite-10624',
+      storageBucket: 'incite-10624.appspot.com',
+      messagingSenderId: '681440609050'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
